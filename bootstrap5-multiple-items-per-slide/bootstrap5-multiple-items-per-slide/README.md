ea33ce15d06486fee845cb51905e99fb926a5185
KhanaTechnologies <137198849+KhanaTechnologies@users.noreply.github.com>
2023-09-08 13:05:13 +0200

    Add files via upload

---
+++ bootstrap5-multiple-items-per-slide/bootstrap5-multiple-items-per-slide/README.md
@@ -0,0 +1,4 @@
+# Bootstrap5 Multiple Items Per Slide
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hellomev/pen/LYORMQW](https://codepen.io/hellomev/pen/LYORMQW).
+
---
+++ script.js
@@ -0,0 +1,50 @@
+let items = document.querySelectorAll('.carousel .carousel-item ')
+
+		items.forEach((el) => {
+			const minPerSlide = 4
+			let next = el.nextElementSibling
+			for (var i=1; i<minPerSlide; i++) {
+				if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+
+
+jQuery(document).ready(function($) {
+    $('#myCarousel3').carousel({
+        interval: 1000000
+    });
+
+    $('#myCarousel').carousel({
+            interval: 3000
+    });
+    $('#myCarousel2').carousel({
+            interval: 2000
+    });
+    $('#myCarousel4').carousel({
+        interval: 2000
+});
+
+    $('#carousel-text').html($('#slide-content-0').html());
+
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click( function(){
+            var id_selector = $(this).attr("id");
+            var id = id_selector.substr(id_selector.length -1);
+            var id = parseInt(id);
+            $('#myCarousel').carousel(id);
+    });
+
+
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid', function (e) {
+            var id = $('.item.active').data('slide-number');
+            $('#carousel-text').html($('#slide-content-'+id).html());
+    });
+});
